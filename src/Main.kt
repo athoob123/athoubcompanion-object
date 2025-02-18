fde353e872e1f47b1882e572584f232e34408149
athoob123 <athoob.alroumi@gmail.com>
2025-02-18 09:41:13 +0300

    companion exercise

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
+class TemperatureConverter {
+    companion object {
+        fun celsiusToFahrenheit(celsius: Double): Double {
+            return (celsius * 9 / 5) + 32
+        }
+        fun fahrenheitToCelsius(fahrenheit: Double): Double {
+            return (fahrenheit - 32) * 5 / 9
+        }
+    }
+}
 fun main() {
-    println("Hello World!")
+    val celsius = 25.0
+    val fahrenheit = 77.0
+    println("$celsius°C is ${TemperatureConverter.celsiusToFahrenheit(celsius)}°F")
+    println("$fahrenheit°F is ${TemperatureConverter.fahrenheitToCelsius(fahrenheit)}°C")
 }
